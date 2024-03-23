a50089eb87061d50ef34d39e9a128b32db5a076e
VO HOANG NAM <153188576+vohoangnam-hcmus@users.noreply.github.com>
2024-03-23 09:36:20 +0700

    Update Main.java

--- src/main/java/vn/neosoft/Main.java
+++ src/main/java/vn/neosoft/Main.java
@@ -3,5 +3,6 @@ package vn.neosoft;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Co su thay doi trong ma nguon.");
     }
 }
