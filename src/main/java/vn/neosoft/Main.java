51e00164fbf0a3e2dfa86f0a0ff207ec593f7a4d
vohoangnam-hcmus <21207182@student.hcmus.edu.vn>
2024-03-23 10:50:35 +0700

    Insert Hello

--- src/main/java/vn/neosoft/Main.java
+++ src/main/java/vn/neosoft/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Co su thay doi trong ma nguon.");
+        System.out.println("hello");
     }
 }
